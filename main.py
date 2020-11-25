742e450b12793434997091044bfc90db287551d3
Racfbie <rachelbiezuner@gmail.com>
2020-11-25 15:53:56 -0300

    Correção 1 da escala

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import numpy as np
 # Configs
 window_name = "mandelbrot"
 WIDTH,HEIGHT = 800, 800
-INFINITO = 100
+INFINITO = 1000
 ITER = 2000
-
+FAC = 0.001
 # -----------------------------------------------------------------------------
 
 input_img = np.zeros((WIDTH,HEIGHT))
@@ -22,12 +22,12 @@ def secMandel(c):
     else:
         return 255
 
-for pixel in input_img:
-    print(pixel[0],pixel[1])
-    pixel = secMandel(complex(pixel[0],pixel[1]))
+for x in range(WIDTH):
+    for y in range(HEIGHT):
+        input_img[x][y] = secMandel(complex(FAC * x, FAC * y))
 
 
 
 plt.title(window_name+" - "+str(input_img.shape))
-plt.imshow(input_img, clim=(0,255))
+plt.imshow(input_img,cmap='gray',clim=(0,255))
 plt.show()
